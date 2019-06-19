f02b0343003a55f26d4ee244b960be2f0bd617fe
Dagttatray Kale <way2datta@gmail.com>
2019-06-19 19:23:22 +0530

    Node express implementation

---
+++ backend/node-express-implementatiion/src/index.js
@@ -0,0 +1,6 @@
+console.log('Hello World!');
+
+// Needed for Hot Module Replacement
+if(typeof(module.hot) !== 'undefined') { // eslint-disable-line no-undef  
+  module.hot.accept()                    // eslint-disable-line no-undef  
+}
