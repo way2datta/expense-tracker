c81ceb5500f44432953feb0b8e3902a642048dad
Dagttatray Kale <way2datta@gmail.com>
2019-07-12 15:27:48 +0530

    should throw an error when validation fails

--- backend/sequelize-impl/src/app.js
+++ backend/sequelize-impl/src/app.js
@@ -3,6 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
+app.post('/login', (req, res) => res.send('Hello login!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
--- backend/sequelize-impl/test/app.test.js
+++ backend/sequelize-impl/test/app.test.js
@@ -6,11 +6,7 @@ let server = require("./../src/app");
 let should = chai.should();
 
 chai.use(chaiHttp);
-//Our parent block
 describe("Routes", () => {
-  /*
-   * Test the /GET route
-   */
   describe("/GET Index", () => {
     it("should GET plain text", done => {
       chai
@@ -20,6 +16,19 @@ describe("Routes", () => {
           res.should.have.status(200);
           res.text.should.be.a('string');
           res.text.should.be.equals('Hello World!');
+          done();
+        });
+    });
+  });
+  describe("Login", () => {
+    it("should throw an error when validation fails", done => {
+      chai
+        .request(server)
+        .post("/login")
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.text.should.be.a('string');
+          res.text.should.be.equals('Hello login!');
           done();
         });
     });
