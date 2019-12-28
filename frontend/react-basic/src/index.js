a3e63a4107fced568f8cf00c54247d7e5db9939b
Dagttatray Kale <way2datta@gmail.com>
2019-12-28 19:49:02 +0530

    React basic implementation of expense tracker

--- frontend/react-basic/src/index.js
+++ frontend/react-basic/src/index.js
@@ -1,13 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Message from './js/Message'
-import './css/style.css'
+import React from 'react';
+import { render } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import * as serviceWorker from './serviceWorker';
+import App from './App';
 
-ReactDOM.render(
-  <Message />,
-  document.getElementById('react-container') // eslint-disable-line no-undef
-)
-// Needed for Hot Module Replacement
-if(typeof(module.hot) !== 'undefined') { // eslint-disable-line no-undef  
-  module.hot.accept()                    // eslint-disable-line no-undef  
-}
+window.APIUrl = "http://localhost:3000/api";
+
+render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>,
+    document.getElementById('root'),
+);
+
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
